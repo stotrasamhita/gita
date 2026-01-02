377332e242de6df87f48171ca11224951629b78f
Karthik Raman <karthikraman@users.noreply.github.com>
2026-01-02 13:18:25 +0530

    updates

--- words/extract_words.sh
+++ words/extract_words.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 grep '^{' gita-words.tex | grep -v '॥' | sed 's/[{]//;s/\}.*//' | tr ' ' '\n' | sed 's/ं$/म्/' | sort | uniq > gita-wordlist.txt
+grep item index_word.tex | awk '{print $1}' | sed 's@\\item\[@@g;s@]$@@g' > words.txt
